6c945f1ff90ba9e82871427b0da4f0b2cdfcb5b9
Arthur Hartwig Carlsson <arthur@kiron.net>
2023-12-04 23:18:37 +0100

    Day 4, part 2

--- days/day04/src/main.rs
+++ days/day04/src/main.rs
@@ -5,7 +5,9 @@ use aoc2023::{extract_number, normalized_lines};
 const INPUT: &'static str = include_str!("../input.txt");
 
 fn main() {
-    println!("Points worth in total: {}", calculate_total_points(INPUT))
+    println!("Points worth in total: {}", calculate_total_points(INPUT));
+
+    println!("Total scratchcards: {}", calculate_scratchcards(INPUT));
 }
 
 fn calculate_total_points(s: &str) -> u32 {
@@ -15,25 +17,44 @@ fn calculate_total_points(s: &str) -> u32 {
         .sum()
 }
 
+fn calculate_scratchcards(s: &str) -> u32 {
+    let cards = normalized_lines(s)
+        .flat_map(|l| Card::from_str(l).into_iter())
+        .collect::<Vec<_>>();
+
+    let mut scratchcards = vec![1; cards.len()];
+    for (i, card) in cards.iter().enumerate() {
+        for n in 0..card.matching_numbers() {
+            scratchcards[i + n + 1] += scratchcards[i];
+        }
+    }
+
+    scratchcards.iter().sum()
+}
+
 struct Card {
-    number: u32,
+    _number: u32,
     winning_numbers: Vec<u32>,
     my_numbers: Vec<u32>,
 }
 impl Card {
     fn points(&self) -> u32 {
+        let matchin_numbers = self.matching_numbers() as u32;
+
+        if matchin_numbers == 0 {
+            0
+        } else {
+            2u32.pow(matchin_numbers - 1)
+        }
+    }
+
+    fn matching_numbers(&self) -> usize {
         let unique_winning_numbers = self.winning_numbers.iter().copied().collect::<HashSet<_>>();
         let unique_my_numbers = self.my_numbers.iter().copied().collect::<HashSet<_>>();
 
-        let intersecting_numbers = unique_winning_numbers
+        unique_winning_numbers
             .intersection(&unique_my_numbers)
-            .count() as u32;
-
-        if intersecting_numbers == 0 {
-            0
-        } else {
-            2u32.pow(intersecting_numbers - 1)
-        }
+            .count()
     }
 }
 impl FromStr for Card {
@@ -54,7 +75,7 @@ impl FromStr for Card {
             .collect::<Vec<_>>();
 
         Ok(Card {
-            number,
+            _number: number,
             winning_numbers,
             my_numbers,
         })
@@ -78,7 +99,7 @@ mod tests {
     fn test_card_points() {
         assert_eq!(
             Card {
-                number: 1,
+                _number: 1,
                 winning_numbers: vec![41, 48, 83, 86, 17],
                 my_numbers: vec![83, 86, 6, 31, 17, 9, 48, 53]
             }
@@ -90,5 +111,20 @@ mod tests {
     #[test]
     fn test_example() {
         assert_eq!(calculate_total_points(EXAMPLE), 13);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        assert_eq!(calculate_scratchcards(EXAMPLE), 30);
+    }
+
+    #[test]
+    fn test_result_part1() {
+        assert_eq!(calculate_total_points(INPUT), 26218);
+    }
+
+    #[test]
+    fn test_result_part2() {
+        assert_eq!(calculate_scratchcards(INPUT), 9997537);
     }
 }
